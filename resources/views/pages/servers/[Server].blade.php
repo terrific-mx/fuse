d3b7eefad9e65c356b30445284b95784e389567d
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-05 09:24:39 -0600

    wip

--- resources/views/pages/servers/[Server].blade.php
+++ resources/views/pages/servers/[Server].blade.php
@@ -1,21 +1,49 @@
 <?php
 
 use App\Models\Server;
+use Illuminate\Database\Eloquent\Collection;
 use Livewire\Volt\Component;
 
 new class extends Component {
     public Server $server;
+    public ?Collection $sites;
+
+    public function mount()
+    {
+        $this->sites = $this->server->sites;
+    }
 }; ?>
 
 <x-layouts.app>
     @volt('pages.servers.show')
-        <section class="space-y-5">
-            <flux:heading>
-                {{ $server->name }}
-            </flux:heading>
+        <section class="space-y-6">
+            <flux:heading>{{ $server->name }}</flux:heading>
+
             <flux:button href="/servers/{{ $server->id }}/sites/create">
                 {{ __('Add site') }}
             </flux:button>
+
+            <flux:table>
+                <flux:table.columns>
+                    <flux:table.column>{{ __('Domain') }}</flux:table.column>
+                    <flux:table.column>{{ __('Status') }}</flux:table.column>
+                    <flux:table.column>{{ __('Actions') }}</flux:table.column>
+                </flux:table.columns>
+
+                <flux:table.rows>
+                    @foreach ($sites as $site)
+                        <flux:table.row>
+                            <flux:table.cell variant="strong">
+                                <flux:link href="/sites/{{ $site->id }}">{{ $site->domain }}</flux:link>
+                            </flux:table.cell>
+                            <flux:table.cell><flux:badge color="green" size="sm" inset="top bottom">{{ $site->status }}</flux:badge></flux:table.cell>
+                            <flux:table.cell>
+                                <flux:link href="/sites/{{ $site->id }}/delete">{{ __('Delete') }}</flux:link>
+                            </flux:table.cell>
+                        </flux:table.row>
+                    @endforeach
+                </flux:table.rows>
+            </flux:table>
         </section>
     @endvolt
 </x-layouts.app>
