119c188fb5beae2b0088e56398067ce2fea623b9
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-06 14:44:59 -0600

    wip

--- resources/views/scripts/application/_make-directories-writable.blade.php
+++ resources/views/scripts/application/_make-directories-writable.blade.php
@@ -1,10 +1,10 @@
 @foreach($writeableDirectories as $directory)
-    DIRECTORY_IS_WRITEABLE=$(getfacl -p {!! $releaseDirectory !!}/{!! $directory !!} | grep "^user:{!! $site->user !!}:.*w" | wc -l)
+    DIRECTORY_IS_WRITEABLE=$(getfacl -p {!! $releaseDirectory !!}/{!! $directory !!} | grep "^user:{!! $application->server->username !!}:.*w" | wc -l)
 
     if [ $DIRECTORY_IS_WRITEABLE -eq 0 ]; then
         # Make the directory writable (without sudo)
-        setfacl -L -m u:{!! $site->user !!}:rwX {!! $releaseDirectory !!}/{!! $directory !!}
-        setfacl -dL -m u:{!! $site->user !!}:rwX {!! $releaseDirectory !!}/{!! $directory !!}
+        setfacl -L -m u:{!! $application->server->username !!}:rwX {!! $releaseDirectory !!}/{!! $directory !!}
+        setfacl -dL -m u:{!! $application->server->username !!}:rwX {!! $releaseDirectory !!}/{!! $directory !!}
     fi
 
 @endforeach
