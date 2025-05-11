ce9882640f61723a1ab6fc045f10b371c9192869
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-11 16:42:07 -0600

    wip

--- tests/Feature/Pages/ServersTest.php
+++ tests/Feature/Pages/ServersTest.php
@@ -2,6 +2,7 @@
 
 use App\Jobs\InstallApplication;
 use App\Jobs\ProvisionServer;
+use App\Models\Application;
 use App\Models\Server;
 use App\Models\ServerProvider;
 use App\Models\SourceProvider;
@@ -398,5 +399,34 @@ describe('create application', function () {
 
         expect($server->applications()->first()->deployments->last())
             ->status->toBe('failed');
+    });
+});
+
+describe('applications', function () {
+    it('allows authenticated users to access the applications page', function () {
+        /** @var User */
+        $user = User::factory()->create();
+
+        actingAs($user)->get('/applications')->assertOk();
+    });
+
+    it('redirects unauthenticated users to the login page when trying to access the applications page', function () {
+        get('/applications')->assertRedirect('/login');
+    });
+
+    it('fetches only the applications associated with the authenticated user', function () {
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
+        $applicationA = Application::factory()->for(Server::factory()->for($user))->create();
+        $applicationB = Application::factory()->for(Server::factory()->for($otherUser))->create();
+        $applicationC = Application::factory()->for(Server::factory()->for($user))->create();
+
+        $component = Volt::actingAs($user)->test('pages.applications');
+
+        expect($component->applications)->toHaveCount(2);
+        expect($component->applications->pluck('id'))
+            ->toContain($applicationA->id)
+            ->toContain($applicationC->id)
+            ->not->toContain($applicationB->id);
     });
 });
