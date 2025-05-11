22aaf2c918d356403669ebc7a3e3ca1be36aa882
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-11 16:51:02 -0600

    wip

--- tests/Feature/Pages/ApplicationsTest.php
+++ tests/Feature/Pages/ApplicationsTest.php
@@ -306,3 +306,41 @@ describe('deployment settings', function () {
         get("applications/{$application->id}/deployment-settings")->assertRedirect('/login');
     });
 });
+
+describe('environment variables', function () {
+    it('creates a task to fetch the environment file', function () {
+        Process::fake();
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.environment-variables', ['application' => $application])
+            ->call('getEnvironmentFile');
+
+        expect($application->server->tasks)->toHaveCount(1);
+        expect($application->server->tasks->first()->name)->toBe('Fetching .env File');
+    });
+
+    it('creates a task to save the environment file', function () {
+        Process::fake();
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.environment-variables', ['application' => $application])
+            ->set('environmentVariables', 'APP_NAME=Fuse')
+            ->call('save');
+
+        expect($application->server->tasks)->toHaveCount(1);
+        expect($application->server->tasks->first()->name)->toBe('Saving .env File');
+    });
+
+    it('validates environment variables before saving', function () {
+        Process::fake();
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.environment-variables', ['application' => $application])
+            ->set('environmentVariables', 12345)
+            ->call('save')
+            ->assertHasErrors(['environmentVariables' => 'string']);
+    });
+});
