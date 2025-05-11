4bb28011580c083e7f9294c55b454d3f2604a775
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-11 16:49:43 -0600

    wip

--- tests/Feature/Pages/ApplicationsTest.php
+++ tests/Feature/Pages/ApplicationsTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\DeleteApplication;
 use App\Jobs\DeployApplication;
 use App\Jobs\UpdateApplicationCaddyFile;
 use App\Models\Application;
@@ -158,5 +159,150 @@ describe('deployments', function () {
         $application = Application::factory()->create();
 
         get("applications/{$application->id}/deployments")->assertRedirect('/login');
+    });
+});
+
+describe('delete', function () {
+    it('can delete an application', function () {
+        Process::fake();
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.delete', ['application' => $application])
+            ->call('delete');
+
+        expect(Application::count())->toBe(0);
+    });
+
+    it('runs script to upade all caddy imports', function () {
+        Process::fake();
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.delete', ['application' => $application])
+            ->call('delete');
+
+        expect($application->server->tasks->first())->name->toBe('Updating Caddy Imports');
+    });
+
+    it('run script to delete application folder', function () {
+        Process::fake();
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.delete', ['application' => $application])
+            ->call('delete');
+
+        expect($application->server->tasks)->toHaveCount(2);
+        expect($application->server->tasks->last())->name->toBe('Deleting folder');
+    });
+
+    it('checks only authorized users can view the delete application page', function () {
+        $application = Application::factory()->create();
+        $user = $application->user();
+        /** @var User */
+        $anotherUser = User::factory()->create();
+
+        actingAs($anotherUser)->get("applications/{$application->id}/delete")->assertForbidden();
+    });
+
+    it('checks unauthenticated users are redirected to login page', function () {
+        $application = Application::factory()->create();
+
+        get("applications/{$application->id}/delete")->assertRedirect('/login');
+    });
+
+    it('dispatches a job to delete the application from the server', function () {
+        Queue::fake();
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.delete', ['application' => $application])
+            ->call('delete');
+
+        Queue::assertPushed(DeleteApplication::class);
+    });
+});
+
+describe('deployment settings', function () {
+    it('can update the deployment settings', function () {
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.deployment-settings', ['application' => $application])
+            ->set('releases_to_retain', 10)
+            ->set('shared_directories', '/path/to/shared/directory')
+            ->set('writable_directories', '/path/to/writable/directory')
+            ->set('shared_files', '/path/to/shared/file')
+            ->set('before_update_hook', 'echo "before update hook"')
+            ->set('after_update_hook', 'echo "after update hook"')
+            ->set('before_activate_hook', 'echo "before activate hook"')
+            ->set('after_activate_hook', 'echo "after activate hook"')
+            ->call('save');
+
+        expect($application->fresh())
+            ->releases_to_retain->toBe(10)
+            ->shared_directories->toBe(['/path/to/shared/directory'])
+            ->writable_directories->toBe(['/path/to/writable/directory'])
+            ->shared_files->toBe(['/path/to/shared/file'])
+            ->before_update_hook->toBe('echo "before update hook"')
+            ->after_update_hook->toBe('echo "after update hook"')
+            ->before_activate_hook->toBe('echo "before activate hook"')
+            ->after_activate_hook->toBe('echo "after activate hook"');
+    });
+
+    it('validates releases_to_retain is required', function () {
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.deployment-settings', ['application' => $application])
+            ->set('releases_to_retain', '')
+            ->call('save')
+            ->assertHasErrors(['releases_to_retain' => 'required']);
+    });
+
+    it('validates releases_to_retain is an integer', function () {
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.deployment-settings', ['application' => $application])
+            ->set('releases_to_retain', 'not an integer')
+            ->call('save')
+            ->assertHasErrors(['releases_to_retain' => 'integer']);
+    });
+
+    it('validates releases_to_retain is at least 1', function () {
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.deployment-settings', ['application' => $application])
+            ->set('releases_to_retain', 0)
+            ->call('save')
+            ->assertHasErrors(['releases_to_retain' => 'min']);
+    });
+
+    it('validates releases_to_retain is at most 50', function () {
+        $application = Application::factory()->create();
+        $user = $application->user();
+
+        Volt::actingAs($user)->test('pages.applications.deployment-settings', ['application' => $application])
+            ->set('releases_to_retain', 51)
+            ->call('save')
+            ->assertHasErrors(['releases_to_retain' => 'max']);
+    });
+
+    it('checks only authorized users can view the deployment settings page', function () {
+        $application = Application::factory()->create();
+        $user = $application->user();
+        /** @var User */
+        $anotherUser = User::factory()->create();
+
+        actingAs($anotherUser)->get("applications/{$application->id}/deployment-settings")->assertForbidden();
+    });
+
+    it('checks unauthenticated users are redirected to login page', function () {
+        $application = Application::factory()->create();
+
+        get("applications/{$application->id}/deployment-settings")->assertRedirect('/login');
     });
 });
