92100534a2e4a6e2874ae0b98c0b806df167c135
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-11 13:56:45 -0600

    wip

--- tests/Feature/Pages/ApplicationsTest.php
+++ tests/Feature/Pages/ApplicationsTest.php
@@ -11,18 +11,18 @@ use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
 
-it('authenticated users can see the applications page', function () {
+it('allows authenticated users to access the applications page', function () {
     /** @var User */
     $user = User::factory()->create();
 
     actingAs($user)->get('/applications')->assertOk();
 });
 
-it('only auhtenticated users can see the applications page', function () {
+it('redirects unauthenticated users to the login page when trying to access the applications page', function () {
     get('/applications')->assertRedirect('/login');
 });
 
-it('only fetch applications for the authenticated user', function () {
+it('fetches only the applications associated with the authenticated user', function () {
     $user = User::factory()->create();
     $otherUser = User::factory()->create();
     $applicationA = Application::factory()->for(Server::factory()->for($user))->create();
