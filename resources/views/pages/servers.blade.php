40a3e22f05bfbc27d82db4c23fbe744e44e6e4a7
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-05-11 14:01:33 -0600

    wip

--- resources/views/pages/servers.blade.php
+++ resources/views/pages/servers.blade.php
@@ -2,19 +2,20 @@
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
+use Laravel\WorkOS\Http\Middleware\ValidateSessionWithWorkOS;
 use Livewire\Volt\Component;
 
 use function Laravel\Folio\middleware;
 
-middleware('auth');
+middleware('auth', ValidateSessionWithWorkOS::class);
 
 new class extends Component {
     public ?Collection $servers;
 
     public function mount()
     {
-        /** @var \App\Models\User $user */
         $user = Auth::user();
+
         $this->servers = $user->servers;
     }
 }; ?>
--- tests/Feature/Pages/ServersTest.php
+++ tests/Feature/Pages/ServersTest.php
@@ -1,5 +1,38 @@
 <?php
 
+use App\Models\Server;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Volt\Volt;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
+
+it('allows authenticated users to access the servers page', function () {
+    /** @var User */
+    $user = User::factory()->create();
+
+    actingAs($user)->get('/servers')->assertOk();
+});
+
+it('redirects unauthenticated users to the login page when trying to access the applications page', function () {
+    get('/servers')->assertRedirect('/login');
+});
+
+it('fetches only the servers associated with the authenticated user', function () {
+    $user = User::factory()->create();
+    $otherUser = User::factory()->create();
+    $serverA = Server::factory()->for($user)->create();
+    $serverB = Server::factory()->for($otherUser)->create();
+    $serverC = Server::factory()->for($user)->create();
+
+    $component = Volt::actingAs($user)->test('pages.servers');
+
+    expect($component->servers)->toHaveCount(2);
+    expect($component->servers->pluck('id'))
+        ->toContain($serverA->id)
+        ->toContain($serverC->id)
+        ->not->toContain($serverB->id);
+});
