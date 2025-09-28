c84ece07dd19f5ae299f1d9eaef001ed8a018541
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-09-27 18:55:07 -0600

    ui: add site logs table and translations for site logs section

--- resources/views/livewire/servers/sites/logs.blade.php
+++ resources/views/livewire/servers/sites/logs.blade.php
@@ -14,6 +14,25 @@ new class extends Component {
     @include('partials.site-navbar', ['server' => $server, 'site' => $site])
 
     <section class="space-y-6">
-        <!-- Add logs details here -->
+        <header>
+            <flux:heading size="lg">{{ __('Site Logs') }}</flux:heading>
+            <flux:text class="mt-2">{{ __('Review important site log files, including all requests made to this site.') }}</flux:text>
+        </header>
+        <flux:table>
+            <flux:table.columns>
+                <flux:table.column class="w-full">{{ __('Log File') }}</flux:table.column>
+                <flux:table.column>{{ __('Description') }}</flux:table.column>
+                <flux:table.column></flux:table.column>
+            </flux:table.columns>
+            <flux:table.rows>
+                <flux:table.row>
+                    <flux:table.cell>Caddy Access Log</flux:table.cell>
+                    <flux:table.cell>{{ __('All requests made to this site') }}</flux:table.cell>
+                    <flux:table.cell align="end">
+                        <flux:button size="sm">{{ __('Access') }}</flux:button>
+                    </flux:table.cell>
+                </flux:table.row>
+            </flux:table.rows>
+        </flux:table>
     </section>
 </div>
