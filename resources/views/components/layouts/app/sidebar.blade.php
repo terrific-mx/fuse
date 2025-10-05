fa722e695de8fb250e006c2b5e908c770b053f8c
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-05 10:03:05 -0600

    ui: highlight Servers nav item when on servers.* route for better UX

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -8,7 +8,7 @@
             <livewire:organizations-dropdown />
 
             <flux:sidebar.nav>
-                <flux:sidebar.item :href="route('servers.index')" icon="server-stack" wire:navigate>{{ __('Servers') }}</flux:sidebar.item>
+                <flux:sidebar.item :href="route('servers.index')" icon="server-stack" :current="request()->routeIs('servers.*')" wire:navigate>{{ __('Servers') }}</flux:sidebar.item>
                 <flux:sidebar.item :href="route('ssh-keys')" icon="key" wire:navigate>{{ __('SSH keys') }}</flux:sidebar.item>
             </flux:sidebar.nav>
 
