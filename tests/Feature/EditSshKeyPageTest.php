279c89db187e36233b62245ac20ba3393a88a73e
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-17 09:40:33 -0600

    core: split SSH key server assignment test into focused cases for assign and unassign

--- tests/Feature/EditSshKeyPageTest.php
+++ tests/Feature/EditSshKeyPageTest.php
@@ -10,15 +10,15 @@ use Livewire\Volt\Volt;
 
 use function Pest\Laravel\actingAs;
 
-test('user can assign and unassign ssh key to servers from volt edit page and jobs are dispatched', function () {
+test('assigning servers to ssh key dispatches authorize jobs', function () {
+    Queue::fake();
+
     $organization = Organization::factory()->create();
     $sshKey = SshKey::factory()->for($organization)->create();
     $servers = Server::factory()->count(3)->for($organization)->create();
 
     actingAs($organization->user);
-    Queue::fake();
 
-    // Assign two servers
     $assigned = $servers->take(2);
     $unassigned = $servers->last();
 
@@ -34,28 +34,41 @@ test('user can assign and unassign ssh key to servers from volt edit page and jo
     });
     expect($sshKey->servers->contains($unassigned))->toBeFalse();
 
-    // Assert jobs dispatched for assigned servers
     $assigned->each(function ($server) use ($sshKey) {
         Queue::assertPushed(AuthorizeSshKeyOnServerJob::class, function ($job) use ($sshKey, $server) {
             return $job->sshKey->is($sshKey) && $job->server->is($server);
         });
     });
+});
 
-    // Now unassign one server (the second assigned one)
-    $stillAssigned = $assigned->first();
-    $toUnassign = $assigned->last();
+test('unassigning servers from ssh key dispatches deauthorize jobs', function () {
+    Queue::fake();
 
+    $organization = Organization::factory()->create();
+    $sshKey = SshKey::factory()->for($organization)->create();
+    $servers = Server::factory()->count(3)->for($organization)->create();
+
+    actingAs($organization->user);
+
+    // Assign all servers first
     Volt::test('ssh-keys.edit', ['sshKey' => $sshKey])
-        ->set('selectedServers', [$stillAssigned->id])
+        ->set('selectedServers', $servers->pluck('id')->toArray())
         ->call('assignServers')
         ->assertHasNoErrors();
+    $sshKey->refresh();
 
+    // Now unassign one server
+    $stillAssigned = $servers->first();
+    $toUnassign = $servers->last();
+    Volt::test('ssh-keys.edit', ['sshKey' => $sshKey])
+        ->set('selectedServers', [$stillAssigned->id, $servers[1]->id])
+        ->call('assignServers')
+        ->assertHasNoErrors();
     $sshKey->refresh();
 
     expect($sshKey->servers->contains($stillAssigned))->toBeTrue();
     expect($sshKey->servers->contains($toUnassign))->toBeFalse();
 
-    // Assert deauthorize job dispatched for unassigned server
     Queue::assertPushed(DeauthorizeSshKeyOnServerJob::class, function ($job) use ($sshKey, $toUnassign) {
         return $job->sshKey->is($sshKey) && $job->server->is($toUnassign);
     });
