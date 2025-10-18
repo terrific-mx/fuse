bdd02ded926514fe3d2147d95e9a36a4a56e6867
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-17 21:09:21 -0600

    core: remove PIPESTATUS propagation from run() remote script execution (exit code now not forced)

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -103,7 +103,7 @@ class Task extends Model
 
         $this->uploadScript();
 
-        $result = $this->executeScriptOnRemoteServer('bash '.$this->remoteScriptPath().' 2>&1 | tee '.$this->fuseDirectory().'/task-'.$this->id.'.log; exit ${PIPESTATUS[0]}');
+        $result = $this->executeScriptOnRemoteServer("bash {$this->remoteScriptPath()} 2>&1 | tee {$this->fuseDirectory()}/task-{$this->id}.log");
 
         $this->update([
             'exit_code' => $result->exitCode(),
