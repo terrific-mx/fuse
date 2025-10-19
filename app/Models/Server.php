248902d60e4e5b4994b439a8963171e001b24d8a
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-18 20:11:31 -0600

    core: simplify cleanup cron install script to use only systemctl reload cron for Ubuntu

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -96,20 +96,19 @@ class Server extends Model
      */
     public function createInstallCleanupCronTask(): Task
     {
+        $script = <<<BASH
+            echo 'SHELL=/bin/sh
+            PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+            0 0 * * * root (find /root/.fuse -name "task-*" -type f -mtime +7 -exec rm {} \; ; find /home/eddy/.fuse -name "task-*" -type f -mtime +7 -exec rm {} \;) 2>&1' > /etc/cron.d/fuse-task-cleanup
+            chmod 644 /etc/cron.d/fuse-task-cleanup
+            systemctl reload cron
+            BASH;
+
         return $this->tasks()->create([
             'name' => 'install_cleanup_cron',
             'user' => 'root',
-            'script' => <<<BASH
-                echo "SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-0 0 * * * root (
-    find /root/.fuse -name \"task-*\" -type f -mtime +7 -exec rm {} \\\; ; find /home/eddy/.fuse -name \"task-*\" -type f -mtime +7 -exec rm {} \\\;
-) 2>&1
-" > /etc/cron.d/fuse-task-cleanup
-                chmod 644 /etc/cron.d/fuse-task-cleanup
-                service cron reload || systemctl reload cron || true
-BASH,
+            'script' => $script,
             'payload' => [],
             'after_actions' => [],
         ]);
