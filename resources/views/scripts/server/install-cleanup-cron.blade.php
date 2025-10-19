c0f5401397cdec7b276752550781d5b919d02b10
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-18 20:19:05 -0600

    core: refactor cleanup cron install to use Blade view and remove temp variable

---
+++ resources/views/scripts/server/install-cleanup-cron.blade.php
@@ -0,0 +1,9 @@
+echo 'SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+0 0 * * * root (\
+    find /root/.fuse -name "task-*" -type f -mtime +7 -exec rm {} \; \
+    find /home/fuse/.fuse -name "task-*" -type f -mtime +7 -exec rm {} \;\
+) 2>&1' > /etc/cron.d/fuse-task-cleanup
+chmod 644 /etc/cron.d/fuse-task-cleanup
+systemctl reload cron
