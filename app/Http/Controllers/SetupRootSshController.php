05e7088e05506a9d4d181b1917f70b803a9b4bca
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-19 19:28:20 -0600

    core: remove unused AuthorizesRequests and authorization from SetupRootSshController

--- app/Http/Controllers/SetupRootSshController.php
+++ app/Http/Controllers/SetupRootSshController.php
@@ -3,16 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Server;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class SetupRootSshController extends Controller
 {
-    use AuthorizesRequests;
-
     public function show(Server $server)
     {
-        $this->authorize('view', $server);
-
         return response(
             view('scripts.server.setup-root-ssh', ['server' => $server])->render()
         )->header('Content-Type', 'text/x-shellscript');
