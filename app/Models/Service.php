dc17b4f1b92bc397c41c11455b4cc0dddc390c77
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-22 08:18:40 -0600

    core: add restart_command to each Service model

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -10,12 +10,12 @@ class Service extends Model
     use Sushi;
 
     protected $rows = [
-        ['name' => 'Caddy 2'],
-        ['name' => 'MySQL 8.0'],
-        ['name' => 'PHP 8.1'],
-        ['name' => 'PHP 8.2'],
-        ['name' => 'PHP 8.3'],
-        ['name' => 'PHP 8.4'],
-        ['name' => 'Redis 6'],
+        ['name' => 'Caddy 2', 'restart_command' => '/usr/sbin/service caddy reload'],
+        ['name' => 'MySQL 8.0', 'restart_command' => 'service mysql restart'],
+        ['name' => 'PHP 8.1', 'restart_command' => 'service php8.1-fpm restart'],
+        ['name' => 'PHP 8.2', 'restart_command' => 'service php8.2-fpm restart'],
+        ['name' => 'PHP 8.3', 'restart_command' => 'service php8.3-fpm restart'],
+        ['name' => 'PHP 8.4', 'restart_command' => 'service php8.4-fpm restart'],
+        ['name' => 'Redis 6', 'restart_command' => 'service redis-server restart'],
     ];
 }
