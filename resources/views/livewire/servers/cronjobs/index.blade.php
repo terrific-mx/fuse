cf26447f5e390a676c68064e353c4c4c5b9334da
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-29 17:48:52 -0600

    ui: refactor cronjobs index page to match database index layout (breadcrumbs, header, spacing, navigation)

--- resources/views/livewire/servers/cronjobs/index.blade.php
+++ resources/views/livewire/servers/cronjobs/index.blade.php
@@ -27,63 +27,68 @@ new class extends Component
     }
 }; ?>
 
-<div wire:poll class="space-y-8">
-    <header class="-mt-6 flex items-center lg:-mt-8">
-        <flux:heading size="lg">{{ $server->name }}</flux:heading>
+<div wire:poll>
+    <flux:breadcrumbs>
+        <flux:breadcrumbs.item :href="route('servers.index')" wire:navigate>Servers</flux:breadcrumbs.item>
+        <flux:breadcrumbs.item :href="route('servers.show', $server)" wire:navigate>
+            {{ $server->name }}
+        </flux:breadcrumbs.item>
+    </flux:breadcrumbs>
+
+    <flux:spacer class="mt-8" />
+
+    <header class="flex items-center">
+        <flux:heading size="xl">Cronjobs</flux:heading>
         <flux:spacer />
-        @include('partials.server-navbar')
+        <flux:button :href="route('servers.cronjobs.create', $server)" variant="primary" color="zinc" wire:navigate>
+            Add cronjob
+        </flux:button>
     </header>
 
-    <section class="mt-12">
-        <div class="flex items-center justify-between">
-            <flux:heading size="xl">Cronjobs</flux:heading>
-            <flux:button :href="route('servers.cronjobs.create', $server)" variant="primary" size="sm" color="zinc">
-                Add cronjob
-            </flux:button>
-        </div>
+    <flux:spacer class="mt-8" />
 
-        <div class="mt-4">
-            <flux:table :paginate="$this->cronjobs">
-                <flux:table.columns>
-                    <flux:table.column>Command</flux:table.column>
-                    <flux:table.column>Frequency</flux:table.column>
-                    <flux:table.column>User</flux:table.column>
-                    <flux:table.column>Status</flux:table.column>
-                    <flux:table.column></flux:table.column>
-                </flux:table.columns>
-                <flux:table.rows>
-                    @foreach ($this->cronjobs as $cronjob)
-                        <flux:table.row :key="$cronjob->id">
-                            <flux:table.cell>{{ $cronjob->command }}</flux:table.cell>
-                            <flux:table.cell>
-                                {{ $cronjob->frequency === 'custom' ? $cronjob->custom_expression : str_replace('_', ' ', ucfirst($cronjob->frequency)) }}
-                            </flux:table.cell>
-                            <flux:table.cell>{{ $cronjob->user }}</flux:table.cell>
-                            <flux:table.cell>{{ ucfirst($cronjob->status) }}</flux:table.cell>
-                            <flux:table.cell align="end">
-                                <flux:button
-                                    :href="route('servers.cronjobs.edit', [$server, $cronjob])"
-                                    variant="subtle"
-                                    size="sm"
-                                    class="mr-2"
-                                >
-                                    Edit
-                                </flux:button>
-                                <flux:button
-                                    wire:confirm="Are you sure you want to delete this cronjob?"
-                                    wire:click="delete({{ $cronjob->id }})"
-                                    :disabled="!$cronjob->isInstalled()"
-                                    inset="top bottom"
-                                    variant="subtle"
-                                    size="sm"
-                                >
-                                    Delete
-                                </flux:button>
-                            </flux:table.cell>
-                        </flux:table.row>
-                    @endforeach
-                </flux:table.rows>
-            </flux:table>
-        </div>
-    </section>
+    <div>
+        <flux:table :paginate="$this->cronjobs">
+            <flux:table.columns>
+                <flux:table.column>Command</flux:table.column>
+                <flux:table.column>Frequency</flux:table.column>
+                <flux:table.column>User</flux:table.column>
+                <flux:table.column>Status</flux:table.column>
+                <flux:table.column></flux:table.column>
+            </flux:table.columns>
+            <flux:table.rows>
+                @foreach ($this->cronjobs as $cronjob)
+                    <flux:table.row :key="$cronjob->id">
+                        <flux:table.cell>{{ $cronjob->command }}</flux:table.cell>
+                        <flux:table.cell>
+                            {{ $cronjob->frequency === 'custom' ? $cronjob->custom_expression : str_replace('_', ' ', ucfirst($cronjob->frequency)) }}
+                        </flux:table.cell>
+                        <flux:table.cell>{{ $cronjob->user }}</flux:table.cell>
+                        <flux:table.cell>{{ ucfirst($cronjob->status) }}</flux:table.cell>
+                        <flux:table.cell align="end">
+                            <flux:button
+                                :href="route('servers.cronjobs.edit', [$server, $cronjob])"
+                                variant="subtle"
+                                size="sm"
+                                class="mr-2"
+                            >
+                                Edit
+                            </flux:button>
+                            <flux:button
+                                wire:confirm="Are you sure you want to delete this cronjob?"
+                                wire:click="delete({{ $cronjob->id }})"
+                                :disabled="!$cronjob->isInstalled()"
+                                inset="top bottom"
+                                variant="subtle"
+                                size="sm"
+                            >
+                                Delete
+                            </flux:button>
+                        </flux:table.cell>
+                    </flux:table.row>
+                @endforeach
+            </flux:table.rows>
+        </flux:table>
+    </div>
 </div>
+
