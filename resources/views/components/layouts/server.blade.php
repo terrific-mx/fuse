0ae7b4b573b44311deed30e9261fad613da3b7a8
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-31 21:08:16 -0600

    ui: add heroicon icons with micro variant to server resources nav menu

--- resources/views/components/layouts/server.blade.php
+++ resources/views/components/layouts/server.blade.php
@@ -9,9 +9,43 @@
             <flux:button icon:trailing="ellipsis-horizontal" size="sm" variant="subtle" />
 
             <flux:menu>
-                <flux:menu.item :href="route('servers.passwords', $server)" wire:navigate icon="key" icon:variant="micro">
-                    Passwords
-                </flux:menu.item>
+                <flux:menu.group heading="Security">
+                    <flux:menu.item :href="route('servers.passwords', $server)" wire:navigate icon="key" icon:variant="micro">
+                        Passwords
+                    </flux:menu.item>
+                </flux:menu.group>
+                <flux:menu.group heading="Configuration">
+                    <flux:menu.item disabled>Caddy</flux:menu.item>
+                    <flux:menu.item disabled>MySQL</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.1 ini</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.1 FPM</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.2 ini</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.2 FPM</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.3 ini</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.3 FPM</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.4 ini</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.4 FPM</flux:menu.item>
+                    <flux:menu.item disabled>Composer auth</flux:menu.item>
+                </flux:menu.group>
+                <flux:menu.group heading="Services">
+                    <flux:menu.item disabled>Caddy</flux:menu.item>
+                    <flux:menu.item disabled>MySQL</flux:menu.item>
+                    <flux:menu.item disabled>Redis</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.1</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.2</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.3</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.4</flux:menu.item>
+                </flux:menu.group>
+                <flux:menu.group heading="Logs">
+                    <flux:menu.item disabled>Caddy access log</flux:menu.item>
+                    <flux:menu.item disabled>Caddy error log</flux:menu.item>
+                    <flux:menu.item disabled>MySQL error log</flux:menu.item>
+                    <flux:menu.item disabled>Redis log</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.1 FPM log</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.2 FPM log</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.3 FPM log</flux:menu.item>
+                    <flux:menu.item disabled>PHP 8.4 FPM log</flux:menu.item>
+                </flux:menu.group>
             </flux:menu>
         </flux:dropdown>
     </header>
@@ -26,10 +60,16 @@
         <flux:navbar class="-mb-px overflow-hidden overflow-x-scroll">
             <flux:navbar.item :href="route('servers.show', $server)" :accent="false" wire:navigate>Home</flux:navbar.item>
             <flux:navbar.item :href="route('servers.sites.index', $server)" :accent="false" wire:navigate>Sites</flux:navbar.item>
-            <flux:navbar.item :href="route('servers.databases.index', $server)" :accent="false" wire:navigate>Databases</flux:navbar.item>
-            <flux:navbar.item :href="route('servers.cronjobs.index', $server)" :accent="false" wire:navigate>Cronjobs</flux:navbar.item>
-            <flux:navbar.item :href="route('servers.firewall-rules.index', $server)" :accent="false" wire:navigate>Firewall rules</flux:navbar.item>
-            <flux:navbar.item :href="route('servers.daemons.index', $server)" :accent="false" wire:navigate>Daemons</flux:navbar.item>
+            <flux:dropdown>
+                <flux:navbar.item icon:trailing="chevron-down">Resources</flux:navbar.item>
+                <flux:navmenu>
+                    <flux:navmenu.item :href="route('servers.databases.index', $server)" :accent="false" wire:navigate icon="circle-stack" icon:variant="micro">Databases</flux:navmenu.item>
+                    <flux:navmenu.item :href="route('servers.cronjobs.index', $server)" :accent="false" wire:navigate icon="clock" icon:variant="micro">Cronjobs</flux:navmenu.item>
+                    <flux:navmenu.item :href="route('servers.daemons.index', $server)" :accent="false" wire:navigate icon="cpu-chip" icon:variant="micro">Daemons</flux:navmenu.item>
+                    <flux:navmenu.item :href="route('servers.firewall-rules.index', $server)" :accent="false" wire:navigate icon="shield-check" icon:variant="micro">Firewall rules</flux:navmenu.item>
+                </flux:navmenu>
+            </flux:dropdown>
+            <flux:navbar.item disabled>Backups</flux:navbar.item>
         </flux:navbar>
     </div>
 
